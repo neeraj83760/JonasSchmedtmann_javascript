48793009879508cacbc126408ddf854bfe73ef11
neeraj <neerajkumarghildiyal@gmail.com>
2023-02-07 03:16:47 +0530

    Completed lecture 235 of mapty project

--- WorkoutMap_Tracker/script.js
+++ WorkoutMap_Tracker/script.js
@@ -12,6 +12,9 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 // we used if condition for just to check wether navigator function exist or not in old browsers or not
+
+let map, mapEvent; 
+
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
     
@@ -25,7 +28,7 @@ if(navigator.geolocation){
 
     const coords = [latitude , longitude]
 //    second parameter is '13' which is zoom in value , like how much you would like to zoom it  
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
     // console.log(map); map is an inbulit function of L namespace 
 // Leaflet gives us L namesapce here having tilelayer, addTO, etc as a methods 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -34,8 +37,43 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
     
-    // mapEvent is created by leaftlet library itself 
-    map.on('click', function(mapEvent){
+    // mapEvent is created by leaftlet library itself. Handling clickson Map 
+    map.on('click', function(mapE){
+        mapEvent = mapE;  
+        form.classList.remove('hidden')
+        inputDistance.focus();
+    //     const {lat, lng} = mapEvent.latlng;
+    //     L.marker([lat ,lng]).addTo(map)
+    //     .bindPopup(L.popup({
+    //         maxWidth:250,
+    //         minWidth:100,
+    //         autoClose:false,
+    //         closeOnClick:false,
+    //         className: 'running-popup'
+    //     })).setPopupContent('Workout')
+    //     .openPopup();    
+    
+    // console.log(mapEvent)   
+
+    })
+    }, function(){
+    
+        alert("Could not get your position!!")
+    })
+    
+    }
+    
+   form.addEventListener('submit', function(e){
+    
+     e.preventDefault(); 
+
+    //  Clear Input Fields 
+
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = " "; 
+
+    // Display marker on the Map once user fill in the details on the left side of the 
+    // form have distance workout data
+
         const {lat, lng} = mapEvent.latlng;
         L.marker([lat ,lng]).addTo(map)
         .bindPopup(L.popup({
@@ -49,11 +87,13 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     
     console.log(mapEvent)   
 
-    })
-    }, function(){
-    
-        alert("Could not get your position!!")
-    })
-    
-    }
-    
+
+   })
+
+
+   inputType.addEventListener('change', function(){
+
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden'); 
+
+   })
